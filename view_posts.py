6abc1e2b2e1a24209a4d731c95c929509a77e8e3
aukaik-nh <natchaya.nhong@gmail.com>
2025-04-30 18:46:33 +0700

    Commented out MongoDB fetch code before pushing

--- view_posts.py
+++ view_posts.py
@@ -1,13 +1,10 @@
 from pymongo import MongoClient
 
-# เชื่อมต่อกับ MongoDB
 client = MongoClient('mongodb://localhost:27017/')
 db = client['smart-db']
 collection = db['posts']
 
-# ดึงข้อมูลทั้งหมดจาก collection
 posts = collection.find()
 
-# แสดงข้อมูลทั้งหมด
 for post in posts:
     print(post)
